8377d7f20882b96fbca49f3f7de2fc3a72302ea5
vklya <glushenkovika@gmail.com>
2023-04-16 12:34:23 +0300

    add work with MongoDB logic

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const mongoose = require('mongoose');
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { DB_HOST } = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => app.listen(3000, () => {
+    console.log("Database connection successful. Use port: 3000")
+  }))
+  .catch(error => {console.log(error.message), process.exit(1)});
